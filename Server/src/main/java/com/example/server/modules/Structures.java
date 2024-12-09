e874fc4b96a011befc3b2879c167cb9a6c11e986
Raffaele Melluso <raffaele.melluso@edu.unito.it>
2024-12-09 14:01:07 +0100

    Svuotata la repository

--- Server/src/main/java/com/example/server/modules/Structures.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.modules;
-/**
- * Classe che contiene le costanti utilizzate dal server
- */
-
-public class Structures {
-    public static final int PORT = 8081; // Porta del server
-    public static final String FILE_PATH = "src/main/resources/data.txt";
-}
